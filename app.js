3d2c36b6783deeac9131a3645b44e2ffac411c18
Tarllis <tarllis@gmail.com>
2025-09-30 12:36:55 -0300

    Segundo Commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do numero secreto!');
+/*alert('Boas vindas ao jogo do numero secreto!');
 let numeroSecreto = 5;
 let chute = prompt('escolha um numero entre 1 e 10');
 
@@ -8,3 +8,13 @@ if (chute == numeroSecreto) {
 
     
 }
+    */
+alert("Estou aprendendo JavaScript!");
+
+// Pede o nome da pessoa e guarda e guarda na variável.
+let nomeUsuario = prompt("Digite seu nome");
+
+// Pede a idade da pessoa e guarda na variável.
+let idadeUsuario = prompt("Digite sua idade");
+
+
