d18348779723542365077a2ca8d9b1a5962936ec
Tarllis Sales <tarllis@gmail.com>
2025-09-30 11:22:47 -0300

    Primeiro Commit Santander /Alura 2025

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do numero secreto!');
+let numeroSecreto = 5;
+let chute = prompt('escolha um numero entre 1 e 10');
+
+if (chute == numeroSecreto) {
+
+    console.log('Isso ai! Você descobriu o numero secreto (5)');
+
+    
+}
